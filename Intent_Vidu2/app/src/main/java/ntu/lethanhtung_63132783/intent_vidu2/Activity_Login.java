82b5633468a8199051913293696f3ec8612053e1
Lê Thanh Tùng <130471358+lthanhtung@users.noreply.github.com>
2024-04-30 12:41:52 +0700

    Intent_vidu2_L3

--- Intent_Vidu2/app/src/main/java/ntu/lethanhtung_63132783/intent_vidu2/Activity_Login.java
+++ Intent_Vidu2/app/src/main/java/ntu/lethanhtung_63132783/intent_vidu2/Activity_Login.java
@@ -1,6 +1,8 @@
 package ntu.lethanhtung_63132783.intent_vidu2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,11 @@ public class Activity_Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void XacNhan(View v)
+    {
+        Intent iHome = new Intent(this, Activity_Home.class);
+        
+        startActivity(iHome);
     }
 }
