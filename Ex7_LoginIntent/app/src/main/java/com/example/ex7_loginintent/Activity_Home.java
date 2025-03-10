cf0b206b3c0fe919170c58ab2cb261775b15e9e7
Lê Thanh Tùng <130471358+lthanhtung@users.noreply.github.com>
2025-03-10 11:06:25 +0700

    Ex7_LoginIntent

---
+++ Ex7_LoginIntent/app/src/main/java/com/example/ex7_loginintent/Activity_Home.java
@@ -0,0 +1,31 @@
+package com.example.ex7_loginintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent iHome = getIntent();
+        String UserName = iHome.getStringExtra("UserName");
+        TextView tvUserName;
+        tvUserName = findViewById(R.id.Tv_UserName);
+        tvUserName.setText(UserName);
+    }
+}
---
+++ Ex7_LoginIntent/app/src/main/java/com/example/ex7_loginintent/Activity_Login.java
@@ -0,0 +1,56 @@
+package com.example.ex7_loginintent;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void XacNhan(View v)
+    {
+
+
+        EditText edtUserName;
+        EditText edtPass;
+        EditText edtMail;
+
+        edtUserName = findViewById(R.id.edtUserName);
+        edtPass = findViewById(R.id.edtPass);
+        edtMail = findViewById(R.id.edtMail);
+
+        String username = edtUserName.getText().toString();
+        String pass = edtPass.getText().toString();
+        String mail = edtMail.getText().toString();
+
+        Intent iHome;
+        //Xử lý đăng nhập
+        if(pass.equals("123"))
+        {
+            iHome = new Intent(this, Activity_Home.class);
+            //Hiểu thị kết quả nhận được lên màn hình Home
+            iHome.putExtra("UserName",username);
+            setResult(RESULT_OK,iHome);
+            startActivity(iHome);
+
+        }
+        else {
+            Toast.makeText(this, "Sai tên đăng nhập hoặc mật khẩu!", Toast.LENGTH_LONG).show();
+        }
+    }
+}
