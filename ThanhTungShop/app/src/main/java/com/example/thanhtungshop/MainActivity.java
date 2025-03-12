92b6487bd2b36572dcbedbff1b60a87383a5974f
Lê Thanh Tùng <130471358+lthanhtung@users.noreply.github.com>
2025-03-12 17:39:51 +0700

    Thanh Tung Shop - Bài Tập Làm Thêm
    Modern Dashboard:
    https://androidknowledge.com/modern-material-dashboard-android-studio/

---
+++ ThanhTungShop/app/src/main/java/com/example/thanhtungshop/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.thanhtungshop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    CardView clothingCard;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.constraintLauout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+//        Tìm điều khiển
+        clothingCard = findViewById(R.id.clothingCard);
+        clothingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ClothingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
