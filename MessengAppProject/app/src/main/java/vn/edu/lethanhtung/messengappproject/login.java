a818aa2436c2269998b7baf847fc755bb961faa4
Lê Thanh Tùng <130471358+lthanhtung@users.noreply.github.com>
2025-05-15 15:38:38 +0700

    ChatAppMobileProject - Day 3
    Create Class login.java --> xử lý ngoại lệ cho các trường Email và Password

--- MessengAppProject/app/src/main/java/vn/edu/lethanhtung/messengappproject/login.java
+++ MessengAppProject/app/src/main/java/vn/edu/lethanhtung/messengappproject/login.java
@@ -1,20 +1,82 @@
 package vn.edu.lethanhtung.messengappproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class login extends AppCompatActivity {
+
+    Button button;
+    EditText email, password;
+    FirebaseAuth auth;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"; //Mẫu Email quy định cho trường Email
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.Logbutton);
+        email = findViewById(R.id.editTextLogEmail);
+        password = findViewById(R.id.editTextLogPassword);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Email = email.getText().toString();
+                String pass = password.getText().toString();
+
+                //Kiểm tra trường Email hoặc password
+                if ((TextUtils.isEmpty(Email))){     //Email trống
+                    Toast.makeText(login.this, "Enter The Email", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(pass)) {     //Password trống
+                    Toast.makeText(login.this, "Enter The Password", Toast.LENGTH_SHORT).show();
+                } else if (!Email.matches(emailPattern)) {       //Email sai định dạng
+                    email.setError("Give Proper Email Address");
+                } else if (password.length() < 6) {  //Quy định số lượng ký tự password > 6
+                    password.setError("More Than Six Characters");
+                    Toast.makeText(login.this, "Password Needs To Be Longer Then Six Characters", Toast.LENGTH_SHORT).show();
+                }else{
+                    //Kiểm tra dữ liệu trên csdl FireBase     // Đăng nhập bằng Email và Password
+                    auth.signInWithEmailAndPassword(Email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                try {
+                                    Intent intent = new Intent(login.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }catch (Exception e){
+                                    Toast.makeText(login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }else {
+                                Toast.makeText(login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
 
     }
 }
